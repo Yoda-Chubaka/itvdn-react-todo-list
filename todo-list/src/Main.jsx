ed8b483e327ced0c13f68511a84b58f0bd59df1b
Anastasiia Polishchuk <polishchuck1818@gmail.com>
2024-03-25 18:48:17 +0200

    Update

---
+++ todo-list/src/Main.jsx
@@ -0,0 +1,60 @@
+import { useState, useEffect } from "react";
+import List from "./components/List";
+import { v4 as uuidv4 } from "uuid";
+
+function Main() {
+
+    const [tasks, setTasks] = useState(() => {
+        const storedTodos = localStorage.getItem("tasks");
+        if (!storedTodos) {
+            return []
+        } else {
+            return JSON.parse(storedTodos)
+        }
+    });
+    const [tasksTitle, setTasksTitle] = useState('');
+
+    useEffect(() => {
+        localStorage.setItem('tasks', JSON.stringify(tasks))
+    }, [tasks]);
+
+    const addTask = (e) => {
+        const storedTodos = JSON.parse(localStorage.getItem("tasks"));
+        if (e.key === "Enter" && e.target.value !=="") {
+            setTasks([
+                ...storedTodos, {
+                    id: uuidv4(),
+                    title: tasksTitle,
+                    status: false
+                }
+            ]);
+            setTasksTitle('');
+        }
+    }
+
+    const date = new Date();
+    const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    const month = monthNames[date.getMonth()];
+    const day = date.getDate();
+    const year = date.getFullYear();
+
+
+    return (
+        <div className="container">
+            <h1>Note your task</h1>
+            <span>{month + " " + day + ", " + year}</span>
+            <div className="input-field">
+                <input type="text" 
+                    value={tasksTitle}
+                    onChange={event => setTasksTitle(event.target.value)}
+                    onKeyDown={addTask}
+                    
+                />
+                <label>Task name</label>
+            </div>
+            <List tasks={tasks} />
+        </div>
+    )
+}
+
+export default Main;
